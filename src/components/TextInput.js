69e50f6a4c4077b10e37452c208ac3db59e87c82
NagaSaiAnurag <70886059+nagasaianurag@users.noreply.github.com>
2021-04-22 11:21:55 +0530

    create bmi app

---
+++ src/components/TextInput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TextInput = (props) => {
+  return (
+    <div>
+      <label>{props.label}</label>
+      <input type="number" value={props.value} placeholder={props.placeholder} onChange={props.handleChange}/>
+    </div>
+  );
+};
+
+export default TextInput;
